fc1261845d8ae6aefe6f8b2bb775ca17cfd943e9
aamargant <55943037+aamargant@users.noreply.github.com>
2021-04-11 14:50:40 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@
 echo "##################################################################"
 echo "# $(tput setaf 2)Downloading source code.... $(tput setaf 7)####################################"
 echo "##################################################################"
-git clone https://github.com/CiscoDevNet/ansible-config-audit.git
-cd ansible-config-audit
+git clone https://github.com/aamargant/cml_test.git
+cd cml_test
 echo ""
 echo "##################################################################"
 echo "# $(tput setaf 2)Installing Pip requirements.... $(tput setaf 7)################################"
@@ -23,7 +23,7 @@ echo ""
 echo "##################################################################"
 echo "# $(tput setaf 2)Source code all downloaded. $(tput setaf 7)####################################"
 echo "# $(tput setaf 2)Next Steps, run: $(tput setaf 7)###############################################"
-echo "# $(tput setaf 4)cd ansible-config-audit $(tput setaf 7)#################################################"
+echo "# $(tput setaf 4)cd cml-test $(tput setaf 7)#################################################"
 echo "# $(tput setaf 4)ansible-playbook 1_audit.yml -C -v $(tput setaf 7)###########################"
 echo "##################################################################"
 
