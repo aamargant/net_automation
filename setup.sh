c9ef1619db821e5ca328b319a86e8c6f8162cf87
aamargant <55943037+aamargant@users.noreply.github.com>
2021-05-21 10:47:50 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,7 @@
-#!/bin/bash
-
 LC_ALL=en_US
-
 export LC_ALL
-
 git clone https://aamargant:Sp18Jv93@github.com/aamargant/cml_ansible.git
-
 cd cml_ansible/
-
 python3.6 -m venv venv
 source venv/bin/activate
 pip install --upgrade pip
