d8f6eb3e25f10516ce2b051985284b004a345986
aamargant <55943037+aamargant@users.noreply.github.com>
2021-05-31 22:35:41 +0200

    Add files via upload

---
+++ netconf_cml/nodes.py
@@ -0,0 +1,24 @@
+core1 = {
+             "address": "10.10.20.177",
+             "port": 830,
+             "username": "cisco",
+             "password": "cisco"
+          }
+core2 = {
+             "address": "10.10.20.178",
+             "port": 830,
+             "username": "cisco",
+             "password": "cisco"
+          }
+core3 = {
+             "address": "10.10.20.179",
+             "port": 830,
+             "username": "cisco",
+             "password": "cisco"
+          }
+core4 = {
+             "address": "10.10.20.180",
+             "port": 830,
+             "username": "cisco",
+             "password": "cisco"
+          }
