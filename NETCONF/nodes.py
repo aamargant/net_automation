5dda02378a832298c754bcc6c36c001a6431b52a
aamargant <55943037+aamargant@users.noreply.github.com>
2021-06-01 00:23:58 +0200

    Add files via upload

---
+++ NETCONF/nodes.py
@@ -0,0 +1,29 @@
+core1 = {
+             "name": "Core 1",
+             "address": "10.10.20.177",
+             "port": 830,
+             "username": "cisco",
+             "password": "cisco"
+          }
+core2 = {
+             "name": "Core 2",
+             "address": "10.10.20.178",
+             "port": 830,
+             "username": "cisco",
+             "password": "cisco"
+          }
+core3 = {
+             "name": "Core 3",
+             "address": "10.10.20.179",
+             "port": 830,
+             "username": "cisco",
+             "password": "cisco"
+          }
+core4 = {
+             "name": "Core 4",
+             "address": "10.10.20.180",
+             "port": 830,
+             "username": "cisco",
+             "password": "cisco"
+          }
+core = [core1, core2, core3, core4]
