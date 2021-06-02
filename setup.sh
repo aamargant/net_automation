d77eb5d18fdc48fb5e72d8de388947d9603fb6ba
aamargant <55943037+aamargant@users.noreply.github.com>
2021-06-02 16:53:36 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
+#!/bin/bash
 LC_ALL=en_US
 export LC_ALL
 git clone https://aamargant:Sp18Jv93@github.com/aamargant/cml_ansible.git
-cd cml_ansible/
+cd cml_ansible
 python3.6 -m venv venv
 source venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 ansible-galaxy collection install -r requirements.yml -p ./
+wget https://devhub.cisco.com/artifactory/open-nxos-agents/9.2.3/x86_64/mtx-openconfig-all-1.0.0.0-9.2.3.lib32_n9000.rpm bootflash: vrf management
+
