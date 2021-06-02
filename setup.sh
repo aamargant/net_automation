5652a277dc8a0a0af2ddc4c97d78f5782283c6a7
aamargant <55943037+aamargant@users.noreply.github.com>
2021-06-02 17:37:02 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-echo "#####################################################"
-echo "# Setting Locals ####################################"
-echo "#####################################################"
+echo""
+echo "##################"
+echo "# Setting Locals #"
+echo "##################"
+echo "$ LC_ALL=en_US"
+echo "$ export LC_ALL"
 LC_ALL=en_US
 export LC_ALL
 
-
+echo""
+echo "######################"
+echo "# Cloning repository #"
+echo "######################"
+echo "git clone https://aamargant:Sp18Jv93@github.com/aamargant/cml_ansible.git"
+git clone https://aamargant:Sp18Jv93@github.com/aamargant/cml_ansible.git
