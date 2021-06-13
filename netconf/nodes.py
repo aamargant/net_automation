715ca4f00bb7dd3a954de5c9ac98c139ca6ef171
aamargant <55943037+aamargant@users.noreply.github.com>
2021-06-13 18:14:07 +0200

    Add files via upload

---
+++ netconf/nodes.py
@@ -0,0 +1,112 @@
+core1 = {
+             "name": "Core 1",
+             "address": "10.10.20.177",
+             "port": 830,
+             "username": "cisco",
+             "password": "cisco"
+          }
+core2 = {
+             "name": "Core 2",
+             "address": "10.10.20.178",
+             "port": 830,
+             "username": "cisco",
+             "password": "cisco"
+          }
+core3 = {
+             "name": "Core 3",
+             "address": "10.10.20.179",
+             "port": 830,
+             "username": "cisco",
+             "password": "cisco"
+          }
+core4 = {
+             "name": "Core 4",
+             "address": "10.10.20.180",
+             "port": 830,
+             "username": "cisco",
+             "password": "cisco"
+          }
+dist1 = {
+             "name": "Distribution 1",
+             "address": "10.10.20.181",
+             "port": 830,
+             "username": "cisco",
+             "password": "cisco"
+          }
+dist2 = {
+             "name": "Distribution 2",
+             "address": "10.10.20.182",
+             "port": 830,
+             "username": "cisco",
+             "password": "cisco"
+          }
+dist3 = {
+             "name": "Distribution 3",
+             "address": "10.10.20.183",
+             "port": 830,
+             "username": "cisco",
+             "password": "cisco"
+          }
+dist4 = {
+             "name": "Distribution 4",
+             "address": "10.10.20.184",
+             "port": 830,
+             "username": "cisco",
+             "password": "cisco"
+          }
+dist5 = {
+             "name": "Distribution 5",
+             "address": "10.10.20.185",
+             "port": 830,
+             "username": "cisco",
+             "password": "cisco"
+          }
+dist6 = {
+             "name": "Distribution 6",
+             "address": "10.10.20.186",
+             "port": 830,
+             "username": "cisco",
+             "password": "cisco"
+          }
+dist8 = {
+             "name": "Distribution 8",
+             "address": "10.10.20.188",
+             "port": 830,
+             "username": "cisco",
+             "password": "cisco"
+          }
+dist9 = {
+             "name": "Distribution 9",
+             "address": "10.10.20.189",
+             "port": 830,
+             "username": "cisco",
+             "password": "cisco"
+          }
+dist10 = {
+             "name": "Distribution 10",
+             "address": "10.10.20.190",
+             "port": 830,
+             "username": "cisco",
+             "password": "cisco"
+          }
+dist11 = {
+             "name": "Distribution 11",
+             "address": "10.10.20.191",
+             "port": 830,
+             "username": "cisco",
+             "password": "cisco"
+          }
+dist12 = {
+             "name": "Distribution 12",
+             "address": "10.10.20.192",
+             "port": 830,
+             "username": "cisco",
+             "password": "cisco"
+          }
+
+core = [core1, core2, core3, core4]
+dist_spine_1 = [dist1, dist2]
+dist_spine_2 = [dist3, dist4]
+dist_spine_3 = [dist5, dist6]
+dist_spine_5 = [dist9, dist10]   
+dist_spine_6 = [dist11, dist12]
